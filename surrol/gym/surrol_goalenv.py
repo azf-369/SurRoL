18a9068c6f97ac79239e8c6e630329667ff69df3
sosoeeee <809953371@qq.com>
2026-02-06 17:20:38 +0800

    compatible to gymnasium

--- surrol/gym/surrol_goalenv.py
+++ surrol/gym/surrol_goalenv.py
@@ -2,6 +2,15 @@ import gym
 from gym import error
 from surrol.gym.surrol_env import SurRoLEnv
 
+# 兼容新旧版本的 gym/gymnasium
+try:
+    import gymnasium
+    from gymnasium import spaces as gym_spaces
+    HAS_GYMNASIUM = True
+except ImportError:
+    from gym import spaces as gym_spaces
+    HAS_GYMNASIUM = False
+
 
 class SurRoLGoalEnv(SurRoLEnv):
     """
@@ -11,8 +20,13 @@ class SurRoLGoalEnv(SurRoLEnv):
 
     def reset(self):
         # Enforce that each GoalEnv uses a Goal-compatible observation space.
-        if not isinstance(self.observation_space, gym.spaces.Dict):
-            raise error.Error('GoalEnv requires an observation space of type gym.spaces.Dict')
+        # 兼容 gym 和 gymnasium 两种版本
+        is_dict_space = isinstance(self.observation_space, gym.spaces.Dict)
+        if HAS_GYMNASIUM:
+            is_dict_space = is_dict_space or isinstance(self.observation_space, gymnasium.spaces.Dict)
+        
+        if not is_dict_space:
+            raise error.Error('GoalEnv requires an observation space of type gym.spaces.Dict or gymnasium.spaces.Dict')
         for key in ['observation', 'achieved_goal', 'desired_goal']:
             if key not in self.observation_space.spaces:
                 raise error.Error('GoalEnv requires the "{}" key to be part of the observation dictionary.'.format(key))
